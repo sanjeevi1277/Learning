cac8069d3c4666d982043ab420bf2fd62dfff677
sanjeevi1277 <66522744+sanjeevi1277@users.noreply.github.com>
2024-09-19 06:41:02 +0530

    Added

---
+++ API/API/DTO's/ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTO_s
+{
+    public class ProductDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateOnly CreatedDate { get; set; } 
+    }
+}
